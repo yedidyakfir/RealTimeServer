f27b5c645d66a949eeb4506f69be880d7be006d1
DavidMeu <mdavid10@gmail.com>
2018-06-10 20:12:56 +0300

    David

--- CenterProject/Client78/ClientWin78.java
+++ CenterProject/Client78/ClientWin78.java
@@ -24,7 +24,7 @@ import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
-
+//
 public class ClientWin78 extends JFrame implements ActionListener,KeyListener
 {
 
